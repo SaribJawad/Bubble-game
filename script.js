7a7191589096855f0b6efce9827e2de79cf72666
Sarib Jawad <132806487+SaribJawad@users.noreply.github.com>
2023-10-11 01:04:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var score = 0;
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").innerHTML = score;
+}
+
+function makeBubble() {
+  var clutter = "";
+
+  for (var i = 1; i <= 144; i++) {
+    let rn = Math.floor(Math.random() * 10);
+
+    clutter += ` <div class="bubble">${rn}</div>`;
+    document.querySelector("#pbtm").innerHTML = clutter;
+  }
+}
+
+var randomNum;
+function randomHit() {
+  randomNum = Math.floor(Math.random() * 10);
+  document.querySelector("#random").innerHTML = randomNum;
+}
+
+var timer = 60;
+function runTimer() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerr").innerHTML = timer;
+    } else {
+      clearInterval(timer);
+      document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h2>`;
+    }
+  }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+  var clickedNum = Number(dets.target.textContent);
+  console.log(clickedNum);
+  if (clickedNum === randomNum) {
+    increaseScore();
+    makeBubble();
+    randomHit();
+  }
+});
+
+randomHit();
+runTimer();
+makeBubble();
